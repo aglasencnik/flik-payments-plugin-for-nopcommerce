61a5ed5ba107f6c69067e174022a811815554132
Amadej Glasenčnik <amadej.glasencnik@gmail.com>
2025-07-29 11:03:39 +0200

    Add plugin defaults

---
+++ Nop.Plugin.Payments.Flik/FlikPaymentDefaults.cs
@@ -0,0 +1,11 @@
+﻿namespace Nop.Plugin.Payments.Flik;
+
+/// <summary>
+/// Represents the default values
+/// </summary>
+public static class FlikPaymentDefaults
+{
+    public static readonly string SystemName = "Payments.Flik";
+
+    public static readonly string ViewsPath = $"~/Plugins/{SystemName}/Views";
+}
